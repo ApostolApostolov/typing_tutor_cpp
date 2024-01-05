fef39a1dd7a11cf2c606523667df1c6d7bd192de
ApostolApostolov <73019184+ApostolApostolov@users.noreply.github.com>
2024-01-05 15:06:47 +0000

    timer
    Needs to be called and improvements to be done

--- Source.cpp
+++ Source.cpp
@@ -11,6 +11,28 @@ using namespace std;
 //constants
 char A_TO_Z_EXCERSISE_STR[] = "abcdefghijklmnopqrstuvwxyz.,";
 
+class Timer {
+public:
+    Timer(size_t time, const std::function<void(void)>& f) : time{std::chrono::milliseconds{time}}, f{f} {}
+    ~Timer() { wait_thread.join(); }
+
+private:
+    void wait_then_call()
+    {
+        std::unique_lock<std::mutex> lck{mtx};
+        for(int i{10}; i > 0; --i) {
+            std::cout << "Thread " << wait_thread.get_id() << " countdown at: " << '\t' << i << '\n';
+            cv.wait_for(lck, time / 10);
+        }
+        f();
+    }
+    std::mutex mtx;
+    std::condition_variable cv{};
+    std::chrono::milliseconds time;
+    std::function <void(void)> f;
+    std::thread wait_thread{[this]() {wait_then_call(); }};
+};
+
 
 
 //universal functions
